3bf7349228c292b610641652e47f176c058405cd
amanfangeria980 <amanfangeria980@gmail.com>
2022-08-19 16:52:47 +0530

    Count the no. of digits by recursion added

---
+++ Recursion/Number-Of-Digits-By-Recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int count(int n){
+    if(n==0){
+        return 0;
+    }
+    return 1+count(n/10);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int result = count(n);
+    cout<<result;
+    return 0;
+}
